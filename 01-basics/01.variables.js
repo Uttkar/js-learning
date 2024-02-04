02e87b26035139e438052733b0e7eaca6a4f01b2
Uttkarsh Pandey <155373706+Uttkar@users.noreply.github.com>
2024-02-04 16:40:37 +0000

    upadte

---
+++ 01-basics/01.variables.js
@@ -0,0 +1,20 @@
+const accountId = 144553
+let accountEmail = "Uttkarshpndey@hotmail.com"
+var accountPassword ="12345"
+accountCity = "Thane"
+let accountstate;
+
+// accountId = 2 not allowed To Chnage Once Created
+/* 
+Prefer not To use For var
+because of issue of block scope and Functional Scope 
+
+*/
+
+
+
+accountEmail = "hell@gmail"
+accountPassword = "Brother"
+accountCity = "Mumbai"
+console.log(accountId);
+console.table([accountId , accountEmail,accountPassword, accountCity, accountstate])

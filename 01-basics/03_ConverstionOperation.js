92c59bbe170406fba585281942bc36817cfe470f
Uttkarsh Pandey <155373706+Uttkar@users.noreply.github.com>
2024-03-03 16:49:13 +0000

    converstion

---
+++ 01-basics/03_ConverstionOperation.js
@@ -0,0 +1,30 @@
+let score = "12dgff"
+
+// if i will put Value that is like (36djk) It will Come Nan That (means Not an Number )
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number (score)
+//  console.log( typeof score)
+// console.log( valueInNumber) 
+
+// "33" => 33 ( it will convert properly)
+// "33abc" => NaN ( it will not Convert any say that this is not a number)
+// true => 1; false => 0
+
+
+let isLoggedIn = ""
+
+let booleanIsLoogedIn = Boolean(isLoggedIn)
+ console.log(booleanIsLoogedIn);
+
+ // 1 => true ; And if I will try to find out whats the value of 0 then It will Gives me False 
+ // "" => false 
+ //  "uttkarsh" => true 
+
+ let someNumber = 33 
+
+ let stringnumber = string(someNumber)
+ console.log(stringnumber);
+ console.log(typeof stringnumber);
